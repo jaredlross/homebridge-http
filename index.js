fc35d3544d0533741a76dd3e5ff911c33dda2b8d
jaredlross <jaredlross@gmail.com>
2017-01-05 14:39:34 -0500

    Update index.js

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 var Service, Characteristic;
 // var request = require("request");
-var request = require('sync-request');
+var request = require("sync-request");
 var pollingtoevent = require('polling-to-event');
 
 	module.exports = function(homebridge){
